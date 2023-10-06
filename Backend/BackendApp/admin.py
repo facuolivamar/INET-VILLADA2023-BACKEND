cb025502d8c27c211d4b7ede7d6b8cd9f94a2ddb
FranciscoGiayetto <frangiayetto@gmail.com>
2023-10-06 14:49:07 -0300

    This commit adds a custom user admin with list display and search fields, a room admin with list display, a doctor admin with list display, a call admin with list display, and a patient admin with list display and search fields. It also registers the custom user, blood type, social plan, room, doctor, calls, and patient models with the admin site. (generated by useblackbox.ai )

--- Backend/BackendApp/admin.py
+++ Backend/BackendApp/admin.py
@@ -1,3 +1,66 @@
 from django.contrib import admin
+from django.db import models
+from .models import *
 
 # Register your models here.
+admin.site.site_header = "Stock"
+admin.site.index_title = "Stock"
+admin.site.site_title = "Stock"
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "grupo",
+    )
+    list_filter = (
+        "groups",
+    )
+    search_fields = ["username", "first_name", "last_name", "email"]
+    def grupo(self, obj):
+        return ", ".join([group.name for group in obj.groups.all()])
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+    'quantity_beds',
+    'room_number'
+    )
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = (
+     "username",
+        "email",
+        "first_name",
+        "last_name",
+        "specialty"
+    )
+class CallAdmin(admin.ModelAdmin):
+    list_display = (
+        "state",
+        "attendance_date",
+        "creation_date",
+        "user_create_call",
+        "user_response_call",
+    )
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "weight",
+        "room",
+        "bloodType",
+        "socialPlan",
+        "height",
+    )
+    search_fields = ["username", "first_name", "last_name", "email"]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(BloodType)
+admin.site.register(SocialPlan)
+admin.site.register(Room,RoomAdmin)
+admin.site.register(Doctor,DoctorAdmin)
+admin.site.register(Calls, CallAdmin)
+admin.site.register(Patient,PatientAdmin)
