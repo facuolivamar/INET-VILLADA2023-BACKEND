303ae673220922b0a13f14769065689c3815c788
facuolivamar <facundoolivam@gmail.com>
2023-10-06 14:12:04 -0300

    CHANNEL LAYER fix to inMemoryChannelLayer

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -130,11 +130,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Backend/settings.py
 # Daphne
 ASGI_APPLICATION = "Backend.asgi.application"
+
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
-        },
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
